41f9831da050bf787c872efeeb5d10358bc2a823
anats <shafiranati@gmail.com>
2022-12-29 21:43:46 +0200

    Add companies collection

---
+++ server/src/collections/companies-collection.js
@@ -0,0 +1,21 @@
+const getDBFunctions = require('../db/db-functions');
+
+const collectionName = 'companies';
+const { insert, findById } = getDBFunctions(collectionName);
+
+const insertCompany = async (company) => await insert(company);
+
+const findCompanyById = async (companyId) => await findById(companyId);
+
+const calcCompanyScore = async (companyId, userId) => {
+  const company = await findById(companyId);
+  const { settings } = await findById(userId);
+  const companyData = { ...company };
+  const { userScoring } = companyData;
+  companyData.userScoringAvg = userScoring.reduce((a, b) => a + b, 0) / userScoring.length;
+  const companyScore = Object.keys(settings)
+    .reduce((score, settingKey) => score + companyData[settingKey] * settings[settingKey], 0);
+  return companyScore;
+};
+
+module.exports = { insertCompany, calcCompanyScore, findCompanyById };
