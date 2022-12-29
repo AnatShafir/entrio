ae519028c4525dcbc5713dc55c622605fb7b4ea5
anats <shafiranati@gmail.com>
2022-12-29 21:45:54 +0200

    Add factory to db functions

--- server/src/db/db-functions.js
+++ server/src/db/db-functions.js
@@ -1,18 +1,24 @@
 const { getDB } = require('./db-client');
 
-const insert = async (collectionName, objToInsert) => {
+const insertFunc = (collectionName) => async (objToInsert) => {
   const collection = getDB().collection(collectionName);
   return await collection.insert(objToInsert);
 };
 
-const findById = async (collectionName, _id) => {
+const findByIdFunc = (collectionName) => async (_id) => {
   const collection = getDB().collection(collectionName);
   return await collection.findOne({ _id });
 };
 
-const updateById = async (collectionName, _id, updateObj) => {
+const updateByIdFunc = (collectionName) => async (_id, updateObj) => {
   const collection = getDB().collection(collectionName);
   return await collection.updateOne({ _id }, { $set: updateObj });
 };
 
-module.exports = { insert, findById, updateById };
+const getDBFunctions = (collectionName) => ({
+  insert: insertFunc(collectionName),
+  findById: findByIdFunc(collectionName),
+  updateById: updateByIdFunc(collectionName),
+});
+
+module.exports = getDBFunctions;
