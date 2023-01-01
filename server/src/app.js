281935b02db70549aa082735842f80ad2668852a
anats <shafiranati@gmail.com>
2023-01-01 15:46:10 +0200

    Add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { port } = require('./config');
 const router = require('./router');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/', router);
 
 module.exports = () => app.listen(port, () => console.log(`App listening on port ${port}`));
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 const startApp = require('./app');
-const { connectDB } = require('./db-interface');
+const { connectDB } = require('./db/db-client');
 const { db: dbConfig } = require('./config');
 
 const start = async () => {
