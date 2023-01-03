1c9385c95a9f7e036229193e1368aff84dbb85ce
anats <shafiranati@gmail.com>
2023-01-03 20:09:00 +0200

    Add flip param hook

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,11 @@ const { connectDB } = require('./db/db-client');
 const logger = require('./utils/logger');
 
 const start = async () => {
+  logger.info('Connecting to the DB...', dbConfig);
   await connectDB(dbConfig);
-  app.listen(port, () => {
-    logger.info(`App is listening on port: ${port}`);
+  logger.info('Db connected successfully');
+  const server = app.listen(port, () => {
+    logger.info(`App is listening on port: ${port}`, server.address());
   });
 };
 
--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,9 +1,21 @@
 const pino = require('pino');
 const { logLevel } = require('../config');
 
+const flipParamsHook = {
+  logMethod(inputArgs, method) {
+    if (inputArgs.length >= 2) {
+      const arg1 = inputArgs.shift();
+      const arg2 = inputArgs.shift();
+      return method.apply(this, [arg2, arg1, ...inputArgs]);
+    }
+    return method.apply(this, inputArgs);
+  },
+};
+
 const logger = pino({
   level: logLevel,
   timestamp: pino.stdTimeFunctions.isoTime,
+  hooks: flipParamsHook,
 });
 
 module.exports = logger;
