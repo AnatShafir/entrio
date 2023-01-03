4be36d2e0c3c971d22abf142ba6ef5f96a57148f
anats <shafiranati@gmail.com>
2023-01-04 01:47:48 +0200

    Remove post new company from toolbar

--- UI/src/app/components/toolbar/toolbar.component.ts
+++ UI/src/app/components/toolbar/toolbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Company } from 'src/app/interfaces/company';
-import { CompaniesService } from 'src/app/services/companies/companies.service';
 import { AddCompanyFormComponent } from '../add-company-form/add-company-form.component';
 
 @Component({
@@ -13,17 +12,13 @@ export class ToolbarComponent {
   header: string = 'Companies';
   @Output() newCompanyEvent = new EventEmitter<Company>();
 
-  constructor(public dialog: MatDialog, private companiesService: CompaniesService) { }
+  constructor(public dialog: MatDialog) { }
 
   openAddCompanyForm(): void {
     const dialogRef = this.dialog.open(AddCompanyFormComponent);
 
     dialogRef.afterClosed().subscribe(newCompany => {
-      console.log(newCompany);
-      // if (newCompany) {
-      //   await this.companiesService.postCompany(newCompany);
-      //   this.newCompanyEvent.emit(newCompany)
-      // }
+      if (newCompany) this.newCompanyEvent.emit(newCompany);
     });
   }
 }
