7b82b9faa74d9b6e0314097de5e3c5c842670e19
anats <shafiranati@gmail.com>
2023-01-04 21:26:17 +0200

    Add logging to companies controller

--- server/src/controllers/companies.controller.js
+++ server/src/controllers/companies.controller.js
@@ -2,9 +2,14 @@ const {
   findAllCompanies, calcCompanyScore, insertCompany,
 } = require('../collections/companies.collection');
 
-const getAllCompanies = async (_req, res, next) => {
+const logger = require('../utils/logger');
+
+const getAllCompanies = async (req, res, next) => {
   try {
+    const { reqId } = req;
+    logger.info('Finding all companies...', { reqId });
     const companies = await findAllCompanies();
+    logger.info('Companies found successfully', { reqId, companiesNumber: companies.length });
     res.status(200).json({ companies });
   } catch (error) {
     next(error);
@@ -13,8 +18,11 @@ const getAllCompanies = async (_req, res, next) => {
 
 const postCompany = async (req, res, next) => {
   try {
+    const { reqId } = req;
     const { company } = req.body;
+    logger.info('Inserting company...', { reqId, company });
     const newCompany = await insertCompany(company);
+    logger.info('Company inserted successfully', { reqId, newCompany });
     res.status(200).json({ company: newCompany });
   } catch (error) {
     next(error);
@@ -23,9 +31,15 @@ const postCompany = async (req, res, next) => {
 
 const getCompanyScore = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { reqId } = req;
+    const { id: companyId } = req.params;
     const { _id: userId } = req.user;
-    const companyScore = await calcCompanyScore(id, userId);
+    logger.info('Calculating company score...', { reqId, companyId, userId });
+    const companyScore = await calcCompanyScore(companyId, userId);
+    logger.info('Company score calculated successfully', {
+      reqId, companyId, userId, companyScore,
+    });
+
     res.status(200).json({ companyScore });
   } catch (error) {
     next(error);
