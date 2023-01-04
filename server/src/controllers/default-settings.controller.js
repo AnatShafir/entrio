204b041ea4c4f2c2dc7653b22a395dbeecf216bf
anats <shafiranati@gmail.com>
2023-01-04 21:31:25 +0200

    Add logger to default settings

--- server/src/controllers/default-settings.controller.js
+++ server/src/controllers/default-settings.controller.js
@@ -1,9 +1,12 @@
 const logger = require('../utils/logger');
 const { findDefaultSettings, updateDefaultSettings } = require('../collections/default-settings.collection');
 
-const getDefaultSettings = async (_, res, next) => {
+const getDefaultSettings = async (req, res, next) => {
   try {
+    const { reqId } = req;
+    logger.info('Finding default settings...', { reqId });
     const settings = await findDefaultSettings();
+    logger.info('Default settings found successfully', { reqId, settings });
     res.status(200).json({ settings });
   } catch (error) {
     next(error);
@@ -12,8 +15,11 @@ const getDefaultSettings = async (_, res, next) => {
 
 const putDefaultSettings = async (req, res, next) => {
   try {
+    const { reqId } = req;
     const { settingsUpdate } = req.body;
+    logger.info('Updating default settings...', { reqId, settingsUpdate });
     await updateDefaultSettings(settingsUpdate);
+    logger.info('Default settings updated successfully', { reqId, settingsUpdate });
     res.status(200).json({ message: 'Updated' });
   } catch (error) {
     next(error);
