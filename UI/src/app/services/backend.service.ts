f5d9b6abc2cd79723092eac7aa03f955c760d6ef
anats <shafiranati@gmail.com>
2023-01-04 14:58:10 +0200

    Add options to http request

--- UI/src/app/services/backend.service.ts
+++ UI/src/app/services/backend.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 
 type Response = Record<string, any>;
@@ -9,21 +9,32 @@ type Response = Record<string, any>;
 })
 export class BackendService {
   private serverUrl = 'http://localhost:3000';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    withCredentials: true
+  };
 
   constructor(private http: HttpClient) { }
 
-  async get(route: string) {
-    const requestObserver = this.http.get<Response>(`${this.serverUrl}/${route}`);
+  applyDefaultOptions(options?: object) {
+    return { ...this.httpOptions, options };
+  }
+
+  async get(route: string, options?: object) {
+    const reqOptions = this.applyDefaultOptions(options);
+    const requestObserver = this.http.get<Response>(`${this.serverUrl}/${route}`, reqOptions);
     return await lastValueFrom(requestObserver);
   }
   
-  async post(route: string, body: object) {
-    const requestObserver = this.http.post<Response>(`${this.serverUrl}/${route}`, body);
+  async post(route: string, body: object, options?: object) {
+    const reqOptions = this.applyDefaultOptions(options);
+    const requestObserver = this.http.post<Response>(`${this.serverUrl}/${route}`, body, reqOptions);
     return await lastValueFrom(requestObserver)
   }
-
-  async patch(route: string, body: object) {
-    const requestObserver = this.http.patch<Response>(`${this.serverUrl}/${route}`, body);
+  
+  async patch(route: string, body: object, options?: object) {
+    const reqOptions = this.applyDefaultOptions(options);
+    const requestObserver = this.http.patch<Response>(`${this.serverUrl}/${route}`, body, reqOptions);
     return await lastValueFrom(requestObserver)
   }
 }
