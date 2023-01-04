05ee6e201f0ed8723c2bc9c79c7b53cf692cc858
anats <shafiranati@gmail.com>
2023-01-04 15:10:11 +0200

    Add authenticate to get company score route

--- server/src/router/companies-router.js
+++ server/src/router/companies-router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const authorize = require('../middleware/authorization');
+const authenticateToken = require('../middleware/authentication');
 const { getAllCompanies, postCompany, getCompanyScore } = require('../controllers/companies-controller');
 
 const companiesRouter = express.Router();
 
 companiesRouter.get('/', getAllCompanies);
 companiesRouter.post('/', postCompany);
-companiesRouter.get('/:id/score/', authorize('user', 'admin'), getCompanyScore);
+companiesRouter.get('/:id/score', authenticateToken, authorize('user', 'admin'), getCompanyScore);
 
 module.exports = companiesRouter;
