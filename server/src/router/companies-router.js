7e87488b9dc4540f55c93481aa2190c5a440297c
anats <shafiranati@gmail.com>
2023-01-04 04:33:29 +0200

    Remove auth middleware in get company score route

--- server/src/router/companies-router.js
+++ server/src/router/companies-router.js
@@ -7,7 +7,7 @@ const userPermitted = authorize('user', 'admin');
 const companiesRouter = express.Router();
 
 companiesRouter.get('/', getAllCompanies);
-companiesRouter.get('/:id/score/:userId', userPermitted, getCompanyScore);
+companiesRouter.get('/:id/score/:userId', getCompanyScore);
 companiesRouter.post('/', postCompany);
 
 module.exports = companiesRouter;
