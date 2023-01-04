6858743fe6f68b9259bebfb84a8f0f2b2d06d401
anats <shafiranati@gmail.com>
2023-01-04 06:25:56 +0200

    Change add request id file name

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const router = require('./router');
 const logHttp = require('./middleware/logger');
-const addReqId = require('./middleware/add-id');
+const addReqId = require('./middleware/add-request-id');
 const errorHandler = require('./middleware/error-handler');
 
 const logger = require('./utils/logger');
