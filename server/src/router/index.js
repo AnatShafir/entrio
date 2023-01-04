c80b656bda860863f40f1902fba536564241232d
anats <shafiranati@gmail.com>
2023-01-04 17:36:46 +0200

    Add default settings router to app

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const companiesRouter = require('./companies.router');
 const usersRouter = require('./users.router');
+const defaultSettingsRouter = require('./default-settings.router');
 
 const router = express.Router();
 const defaultRoute = (_, res) => res.status(404).json({ message: 'Not Found' });
 
 router.use('/company', companiesRouter);
 router.use('/user', usersRouter);
+router.use('/default-settings', defaultSettingsRouter);
 router.use('*', defaultRoute);
 
 module.exports = router;
